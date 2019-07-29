05440bf30ca994b229bed022ac8620d191d68f17
saulius saltenis <ltlion@live.com>
2019-07-29 12:12:46 +0300

    Discord types for database.

---
+++ DiscordSQL/Emoji.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using LionLibrary.SQL;
+
+namespace DiscordSQL
+{
+    [Table("emoji")]
+    public class Emoji : IEntity<Emoji, string>
+    {
+        [Key] [Column("id")]
+        public string Id { get; set; }
+
+        [Required] [Column("name")]
+        public string Name { get; set; }
+
+        [Column("user?")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        [Column("require_colons?")]
+        public bool RequiresColons { get; set; } = false;
+
+        [Column("managed?")]
+        public bool IsManaged { get; set; } = false;
+
+        [Column("animated?")]
+        public bool IsAnimated { get; set; } = false;
+    }
+}
---
+++ DiscordSQL/User.cs
@@ -0,0 +1,59 @@
+﻿using LionLibrary.SQL;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DiscordSQL
+{
+    [Table("user")]
+    public class User : IEntity<User, string>
+    {
+        [Key]
+        [Column("id")]
+        public string Id { get; set; }
+
+        [Required]
+        [Column("username")]
+        public string Username { get; set; }
+
+        [Column("discriminator")]
+        public string Discriminator { get; set; }
+
+        [Column("avatar")]
+        public string Avatar { get; set; }
+
+        [Column("bot?")]
+        public bool? IsBot { get; set; }
+
+        [Column("mfa_enabled?")]
+        public bool MfaEnabled { get; set; }
+
+        [Column("locale?")]
+        public bool Locale { get; set; }
+
+        [Column("verified?")]
+        public bool Verified { get; set; }
+
+        [Column("email?")]
+        public string Email { get; set; }
+
+        [Column("flags?")]
+        public UserFlags Flags { get; set; }
+
+        [Column("premium_type?")]
+        public int PremiumType { get; set; }
+
+        public ICollection<Emoji> Emojis { get; set; }
+
+        public static void CreateModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(x =>
+            {
+                x.HasMany(user => user.Emojis)
+                .WithOne(emoji => emoji.User)
+                .HasForeignKey(emoji => emoji.UserId);
+            });
+        }
+    }
+}
