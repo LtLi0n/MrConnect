c0f8d105b0aa423cb6b8bddb93162e56ee0cb168
LtLi0n <31516441+LtLi0n@users.noreply.github.com>
2019-08-15 00:11:21 +0300

    Removed useless interface.

--- MrConnect/Services/DiscordService.cs
+++ MrConnect/Services/DiscordService.cs
@@ -2,29 +2,33 @@
 using Discord.Commands;
 using Discord.WebSocket;
 using LionLibrary.Framework;
+using LionLibrary.Network;
 using MrConnect.Boot;
+using SharedDiscord;
 using System;
 using System.Reflection;
 using System.Threading.Tasks;
 
 namespace MrConnect.Services
 {
-    public class DiscordService : IDiscordService
+    public class DiscordService
     {
-        private readonly IAppConfig _config;
+        private readonly AppConfig _config;
         private readonly ILogService _logger;
         private readonly IServiceProvider _services;
+        private readonly DiscordConnector _discordConn;
 
         public DiscordSocketClient Client { get; }
         public CommandService Commands { get; }
 
-        public DiscordService(IServiceProvider services, IAppConfig config, ILogService logger)
+        public DiscordService(IServiceProvider services, AppConfig config, ILogService logger, DiscordConnector discordConn)
         {
             _services = services;
             _config = config;
             _logger = logger;
             Client = new DiscordSocketClient(new DiscordSocketConfig { LogLevel = global::Discord.LogSeverity.Verbose });
             Commands = new CommandService();
+            _discordConn = discordConn;
         }
 
         public async Task InstallCommandsAsync()
@@ -43,7 +47,10 @@ namespace MrConnect.Services
 
         private Task Log(LogMessage message)
         {
-            _logger.LogLine(this, message.Message, FromDiscordLogSeverity(message.Severity));
+            if (!string.IsNullOrEmpty(message.Message))
+            {
+                _logger.LogLine(this, message.Message, FromDiscordLogSeverity(message.Severity));
+            }
             return Task.CompletedTask;
         }
 
@@ -59,10 +66,10 @@ namespace MrConnect.Services
                 _ => LionLibrary.Framework.LogSeverity.Info
             };
 
-        private async Task HandleCommandAsync(SocketMessage messageParam)
+        private async Task HandleCommandAsync(SocketMessage msg)
         {
             // Don't process the command if it was a system message
-            SocketUserMessage message = messageParam as SocketUserMessage;
+            SocketUserMessage message = msg as SocketUserMessage;
             if (message == null) return;
 
             // Create a number to track where the prefix ends and the command begins
@@ -70,9 +77,35 @@ namespace MrConnect.Services
 
             // Determine if the message is a command based on the prefix and make sure no bots trigger commands
             if (!(message.HasStringPrefix(_config["discord:prefix"], ref argPos) ||
-                message.HasMentionPrefix(Client.CurrentUser, ref argPos)) ||
-                message.Author.IsBot)
-                return;
+                message.HasMentionPrefix(Client.CurrentUser, ref argPos)))
+            {
+                if(!message.Author.IsBot)
+                {
+                    /*_discordConn.Client.SendPacket(x =>
+                    {
+                        x.Header = "user.add";
+                        x[Id] = msg.Author.Id;
+                        x["Username"] = msg.Author.Username;
+                        x["Discriminator"] = msg.Author.Discriminator;
+                    });
+
+                    _discordConn.Client.SendPacket(x =>
+                    {
+                        x.Header = "message.add";
+                        x[Id] = msg.Id;
+                        x[ChannelId] = msg.Channel.Id;
+                        x[UserId] = msg.Author.Id;
+                        x[Content] = msg.Content;
+                        x[TimeStamp] = msg.Timestamp.ToUnixTimeSeconds();
+                        if (msg.Channel is SocketGuildChannel guildChannel)
+                        {
+                            x[GuildId] = guildChannel.Guild.Id;
+                        }
+                    });*/
+
+                    return;
+                }
+            }
 
             // Create a WebSocket-based command context based on the message
             var context = new SocketCommandContext(Client, message);
