86f614e57c77c85d94ae22156936ae5b02c6a6f4
LtLi0n <LtLi0n@users.noreply.github.com>
2019-10-07 01:54:36 +0300

    Check if a given string contains only numbers. Sure can be done with \d+ regex, whatever really.

---
+++ MrConnect/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace MrConnect
+{
+    public static class StringExtensions
+    {
+        public static bool IsNumber(this string str)
+        {
+            foreach (char ch in str)
+            {
+                if (ch > '9' || ch < '0')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
