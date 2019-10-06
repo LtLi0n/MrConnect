efa4036e2b3f0f9bba0e3177964055fa5f5f7460
LtLi0n <LtLi0n@users.noreply.github.com>
2019-10-06 15:14:27 +0300

    Moved SharedRef to DataServerHelpers assembly.

---
+++ DataServerHelpers/SharedRef.cs
@@ -0,0 +1,10 @@
+﻿namespace DataServerHelpers
+{
+    public static class SharedRef
+    {
+        public const string Id = "id";
+        public const string Select = "select";
+        public const string Where = "where";
+        public const string Page = "page";
+    }
+}
