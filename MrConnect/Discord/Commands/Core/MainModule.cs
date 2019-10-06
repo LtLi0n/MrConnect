02a1f2159300a7a9aee1a989b4a0a22aa2c05a48
LtLi0n <LtLi0n@users.noreply.github.com>
2019-10-07 01:54:59 +0300

    User info command ported from MrDestructoid.

--- MrConnect/Discord/Commands/Core/MainModule.cs
+++ MrConnect/Discord/Commands/Core/MainModule.cs
@@ -8,10 +8,13 @@ using MrConnect.Services;
 using DataServerHelpers;
 using WoT.Shared;
 using LionLibrary.Network;
+using System.Linq;
+using Discord.WebSocket;
+using System;
+using System.Text;
 
 namespace MrConnect.Discord
 {
-    [Group("main")]
     public class MainModule : ModuleBase<SocketCommandContext>
     {
         public AppConfig Config { get; set; }
@@ -21,6 +24,166 @@ namespace MrConnect.Discord
         public async Task Ping()
         {
             await ReplyAsync("pong!");
+        }
+
+        [Command("say")]
+        public async Task SayAsync([Remainder]string text)
+        {
+            await Context.Message.DeleteAsync();
+            await ReplyAsync(text);
+        }
+
+        [Command("user")]
+        [Summary(
+            "%Regular%%info%User info command.%details%\n" +
+            "Info about you or targetted user.\n" +
+            "%usage%`%prefix%%cmd% ([user_name/@mention/id])`\n" +
+            "%examples%\t\t`%prefix%%cmd%` -- Shows info about you.\n" +
+            "\t\t`%prefix%%cmd% Bob123` -- Shows info about targetted user.§")]
+        [RequireContext(ContextType.Guild)]
+        public async Task DisplayUserInfoAsync([Remainder] string target = null)
+        {
+            IGuildUser showUser = Context.User as IGuildUser;
+
+            if (Context.Message.MentionedUsers.Count > 0)
+            {
+                showUser = (IGuildUser)Context.Message.MentionedUsers.First();
+            }
+            else if (target != null)
+            {
+                var users = Context.GetAddressedUsers(target);
+
+                showUser = users.FirstOrDefault();
+
+                if (showUser == null)
+                {
+                    throw new Exception("Didn't find such user. Perhaps try a @Mention?");
+                }
+                else if (users.Count() > 1)
+                {
+                    StringBuilder sb = new StringBuilder("Found too many users. Type exact name or @Mention the user instead.\n");
+                    foreach (var user in users)
+                    {
+                        sb.Append($"{user.Username}{user.Discriminator}\n");
+                    }
+                    throw new Exception(sb.ToString());
+                }
+            }
+
+            EmbedBuilder eb = new EmbedBuilder();
+
+            eb.WithThumbnailUrl(showUser.GetAvatarUrl());
+
+            bool new_user_state = false;
+
+            eb.WithAuthor((x) =>
+            {
+                x.IconUrl = showUser.Status switch
+                {
+                    UserStatus.Online => $"https://cdn.discordapp.com/emojis/630527350173466655.png",
+                    UserStatus.DoNotDisturb => "https://cdn.discordapp.com/emojis/630527350286843904.png",
+                    UserStatus.Offline => "https://cdn.discordapp.com/emojis/630527349917745153.png",
+                    UserStatus.Invisible => "https://cdn.discordapp.com/emojis/630527349917745153.png",
+                    UserStatus.Idle => "https://cdn.discordapp.com/emojis/630536692654145556.png",
+                    UserStatus.AFK => "https://cdn.discordapp.com/emojis/630536692654145556.png",
+                    _ => ""
+                };
+
+                if(string.IsNullOrEmpty(x.IconUrl))
+                {
+                    new_user_state = true;
+                }
+
+                x.Name = $"{showUser.Username}";
+            });
+
+            if (showUser.RoleIds.Count > 0)
+            {
+                eb.Color = Context.Guild.GetRole(showUser.RoleIds.First()).Color;
+
+                List<IRole> userRoles = new List<IRole>();
+
+                foreach (ulong roleID in showUser.RoleIds)
+                {
+                    bool skipRole = true;
+
+                    if ((Context.Guild.GetRole(roleID)).IsEveryone) continue;
+
+                    switch (roleID)
+                    {
+                        case 349875313003724800: break;
+                        case 349875385342885889: break;
+                        case 349875417106087936: break;
+
+                        default:
+                            skipRole = false;
+                            break;
+                    }
+
+                    if (skipRole) continue;
+
+                    userRoles.Add(Context.Guild.GetRole(roleID));
+                }
+
+                IEnumerable<IRole> sortedRoles = userRoles.OrderByDescending(x => x.Position);
+
+                EmbedFieldBuilder roleField = new EmbedFieldBuilder
+                {
+                    Name = string.Format("Role{0}", sortedRoles.Count() > 1 ? $"s [{sortedRoles.Count()}]" : string.Empty)
+                };
+
+                foreach (IRole role in sortedRoles)
+                {
+                    roleField.Value += string.Format("{0}{1}", role.Name, sortedRoles.Last().Id == role.Id ? string.Empty : ", ");
+                }
+
+                eb.Color = sortedRoles.First().Color;
+
+                eb.AddField(roleField);
+            }
+
+            if (showUser.Activity != null)
+            {
+                eb.Description = showUser.Activity.ToString();
+            }
+
+            eb.Fields = new List<EmbedFieldBuilder>
+            {
+                new EmbedFieldBuilder
+                {
+                    Name = "Username",
+                    Value = $"{showUser.Username}#{showUser.Discriminator}",
+                    IsInline = true
+                },
+                new EmbedFieldBuilder
+                {
+                    Name = "Nickname",
+                    Value = showUser.Nickname == null ? "Not Set" : $"{showUser.Nickname}",
+                    IsInline = true
+                },
+                new EmbedFieldBuilder
+                {
+                    Name = "Joined Server At",
+                    Value = showUser.JoinedAt.ToString(),
+                    IsInline = true
+                },
+                new EmbedFieldBuilder
+                {
+                    Name = "Joined Discord At",
+                    Value = showUser.CreatedAt.ToString(),
+                    IsInline = true
+                },
+            };
+
+            eb.Footer = new EmbedFooterBuilder { Text = $"Name:  Id: {showUser.Id}" };
+
+            string msg = "";
+            if(new_user_state)
+            {
+                msg = $"Inform about this. `new UserStatus state needs new icon at {Config.Prefix}user`.";
+            }
+
+            await Context.Channel.SendMessageAsync(msg, embed: eb.Build());
         }
 
         private void ReflectServerResponse(EmbedFieldBuilder efb, StatusCode state) =>
