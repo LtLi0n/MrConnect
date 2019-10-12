b893da99eba2f7ff4549f473c50b97cf2920a4e9
LtLi0n <LtLi0n@users.noreply.github.com>
2019-10-12 20:44:59 +0300

    Added WorkService.

---
+++ MicroServices/WoT/Server/Services/CharacterWork/CharacterWorkService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using WoT.Shared;
+using LionLibrary.Framework;
+
+namespace WoT.Server
+{
+    public class CharacterWorkService : IWoTService
+    {
+        public ILogService Logger { get; }
+        public event EventHandler<CharacterWorkFinishedEventArgs> WorkFinished;
+
+        public CharacterWorkService(ILogService logger)
+        {
+            Logger = logger;
+        }
+        
+        public async Task UpdateAsync(IServiceProvider services)
+        {
+            WoTDbContext db = services.GetService<WoTDbContext>();
+            IQueryable<CharacterWork> workers = db.CharactersWork.Where(x => x.IsWorking);
+
+            foreach(CharacterWork worker in workers)
+            {
+                await HandleWorker(db, worker);
+            }
+        }
+
+        ///<summary>Finish work if it's due to be done.</summary>
+        private async Task HandleWorker(WoTDbContext db, CharacterWork worker)
+        {
+            var args = await worker.UpdateAsync(db, db.Users, db.Characters);
+
+            if(args != null)
+            {
+                WorkFinished?.Invoke(worker, args);
+                Logger?.LogLine(this, args.ToString(), LogSeverity.Verbose);
+            }
+        }
+    }
+}
---
+++ MicroServices/WoT/Shared/Models/CharacterWorkFinishedEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WoT.Shared
+{
+    public class CharacterWorkFinishedEventArgs : EventArgs
+    {
+        public User User { get; }
+        public Character Character { get; }
+        public ICharacterEntity<CharacterWork> Entity { get; }
+        public uint Hours { get; }
+        public uint Reward { get; }
+
+        public CharacterWorkFinishedEventArgs(
+            User user,
+            Character character,
+            ICharacterEntity<CharacterWork> entity,
+            uint hours,
+            uint reward)
+        {
+            User = user;
+            Character = character;
+            Entity = entity;
+            Hours = hours;
+            Reward = reward;
+        }
+
+        public override string ToString()
+        {
+            return $"Character [{Character.Name}] just finished their work! +{Reward} Gold for {Hours} hours of work.";
+        }
+    }
+}
