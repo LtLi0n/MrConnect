acdd6fdfa7803c747ef7a623183feffb9419dc8e
LtLi0n <LtLi0n@users.noreply.github.com>
2019-10-17 02:11:12 +0300

    Using outer group modules, they are so cute.

--- MrConnect/Discord/Commands/WoTModule.cs
+++ MrConnect/Discord/Commands/WoTModule.cs
@@ -24,52 +24,6 @@ namespace MrConnect.Discord
         public CharacterApi CharacterApi => WoT.GetController<CharacterApi>();
         public CharacterWorkApi CharacterWorkApi => WoT.GetController<CharacterWorkApi>();
 
-        [Group("owner"), RequireOwner]
-        public class WoTOwnerModule : ModuleBase<SocketCommandContext>
-        {
-            public AppConfig Config { get; set; }
-            public WoTConnector WoT { get; set; }
-
-            public UserApi UserApi => WoT.GetController<UserApi>();
-            public CharacterApi CharacterApi => WoT.GetController<CharacterApi>();
-            public CharacterWorkApi CharacterWorkApi => WoT.GetController<CharacterWorkApi>();
-
-            [Command("cmd", RunMode = RunMode.Async)]
-            public async Task CmdAsync([Remainder]string query)
-            {
-                string[] inputs = query.Split(' ');
-
-                Packet response = await WoT.DownloadPacketAsync(x =>
-                {
-                    x.Header = inputs[0];
-
-                    //parse args
-                    if (query.TrimEnd().Length > x.Header.Length)
-                    {
-                        string arg_str = query.Substring(x.Header.Length);
-                        string regex_str = @$" *([^=]+)=(?(?="")(""([^""]+)"")|([^ ]+))";
-                        MatchCollection arg_matches = Regex.Matches(arg_str, regex_str);
-                        foreach (Match match in arg_matches)
-                        {
-                            if (match.Success)
-                            {
-                                x.Args.Add(match.Groups[1].Value, (match.Groups[2].Success ? match.Groups[2] : match.Groups[3]).Value);
-                            }
-                        }
-                    }
-                });
-
-                string msg = response.Content;
-
-                if (response.ContentType == PacketContentType.JSON)
-                {
-                    msg = $"```json\n{JToken.Parse(response.Content).ToString()}```";
-                }
-
-                await ReplyAsync(msg);
-            }
-        }
-
         [Command("work"), Alias("w")]
         public async Task HandleWorkCommandAsync()
         {
