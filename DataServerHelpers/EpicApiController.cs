7d030bbb401f7ca059480cdf781a60e4c366c20a
LtLi0n <LtLi0n@users.noreply.github.com>
2019-11-08 23:20:24 +0200

    Wrapping my own wrappers, that's pretty epic.

---
+++ DataServerHelpers/EpicApiController.cs
@@ -0,0 +1,35 @@
+﻿using LionLibrary.Network;
+
+using static DataServerHelpers.SharedRef;
+
+namespace DataServerHelpers
+{
+    public abstract class EpicApiController<EntityT, KeyType> : ApiController, IApiControllerCRUD<EntityT, KeyType>
+        where EntityT : class
+    {
+        public string IdTag => Id;
+        public string SelectTag => Select;
+        public string WhereTag => Where;
+
+        public string FullModulePath { get; }
+
+        public string AddRoute { get; }
+        public string GetRoute { get; }
+        public string ModifyRoute { get; }
+        public string RemoveRoute { get; }
+
+        public IApiControllerCRUD<EntityT, KeyType> CRUD => this;
+
+        public EpicApiController(ServerConnector conn, string fullModulePath) : base(conn)
+        {
+            FullModulePath = fullModulePath;
+
+            AddRoute = $"{fullModulePath}.add";
+            GetRoute = $"{fullModulePath}.get";
+            ModifyRoute = $"{fullModulePath}.modify";
+            RemoveRoute = $"{fullModulePath}.remove";
+        }
+
+        public abstract void FillPacketBucket(PacketBuilder pb, EntityT entity);
+    }
+}
