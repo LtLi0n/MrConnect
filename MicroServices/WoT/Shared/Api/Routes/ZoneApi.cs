6f08c561645756e76a637b2b892279fefe95aa0b
LtLi0n <LtLi0n@users.noreply.github.com>
2019-11-08 23:47:09 +0200

    Adding Zone and ZoneNode ApiControllers.

---
+++ MicroServices/WoT/Shared/Api/Routes/ZoneApi.cs
@@ -0,0 +1,22 @@
+﻿using DataServerHelpers;
+using LionLibrary.Network;
+
+using static WoT.Shared.Zone.Ref;
+
+namespace WoT.Shared
+{
+    public class ZoneApi : EpicApiController<Zone, uint>
+    {
+        public const string MODULE = "zones";
+
+        public ZoneApi(WoTConnector conn) : base(conn, MODULE) { }
+
+        public override void FillPacketBucket(PacketBuilder pb, Zone entity)
+        {
+            pb[IdTag] = entity.Id;
+            pb[CodeName] = entity.CodeName;
+            pb[Name] = entity.Name;
+            pb[Description] = entity.Description;
+        }
+    }
+}
---
+++ MicroServices/WoT/Shared/Api/Routes/ZoneNodeApi.cs
@@ -0,0 +1,28 @@
+﻿using DataServerHelpers;
+using LionLibrary.Network;
+
+using static WoT.Shared.ZoneNode.Ref;
+
+namespace WoT.Shared
+{
+    public class ZoneNodeApi : EpicApiController<ZoneNode, uint>
+    {
+        public const string MODULE = "zones";
+        public static string MODULE_ABSOLUTE = $"{ZoneApi.MODULE}.{MODULE}";
+
+        public ZoneNodeApi(WoTConnector conn) : base(conn, MODULE_ABSOLUTE) { }
+
+        public override void FillPacketBucket(PacketBuilder pb, ZoneNode entity)
+        {
+            pb[ZoneId] = entity.ZoneId;
+
+            pb[ZoneNodeNorthId] = entity.ZoneNodeNorthId;
+            pb[ZoneNodeEastId] = entity.ZoneNodeEastId;
+            pb[ZoneNodeSouthId] = entity.ZoneNodeSouthId;
+            pb[ZoneNodeWestId] = entity.ZoneNodeWestId;
+
+            pb[Name] = entity.Name;
+            pb[Description] = entity.Description;
+        }
+    }
+}
