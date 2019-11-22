f590d2e5658b2479d226bbcc791184174c5e9c4e
LtLi0n <LtLi0n@users.noreply.github.com>
2019-11-22 02:53:31 +0200

    Adding interactive command container support.

---
+++ MrConnect.Shared/Commands/Container/InteractiveCommandContainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MrConnect.Shared
+{
+    ///<summary>For storing interactive commands.</summary>
+    /// <typeparam name="T"></typeparam>
+    public class InteractiveCommandContainer<T>
+        where T : InteractiveCommandContext
+    {
+        internal Dictionary<Type, InteractiveCommand<T>> _commands;
+        internal HashSet<InteractiveCommandBuilder<T>> _builders;
+
+        public InteractiveCommandContainer()
+        {
+            _commands = new Dictionary<Type, InteractiveCommand<T>>();
+            _builders = new HashSet<InteractiveCommandBuilder<T>>();
+        }
+
+        ///<summary>Makes it very easy to inject builders within ServiceCollection.</summary>
+        public InteractiveCommandContainer<T> WithBuilders(Action<InteractiveCommandContainerBuildActionContext<T>> builderAction)
+        {
+            var builder = new InteractiveCommandContainerBuildActionContext<T>();
+            builderAction(builder);
+            builder.InjectInto(this);
+            return this;
+        }
+
+        public void Build()
+        {
+            foreach (var builder in _builders)
+            {
+                _commands.Add(builder.GetType(), builder.Build());
+            }
+
+            _builders.Clear();
+        }
+
+        public InteractiveCommand<T> GetByBuilder<U>() 
+            where U : InteractiveCommandBuilder<T> =>
+            _commands.TryGetValue(typeof(U), out InteractiveCommand<T> cmd) ? cmd : null;
+    }
+}
---
+++ MrConnect.Shared/Commands/Container/InteractiveCommandContainerBuildActionContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace MrConnect.Shared
+{
+    ///<summary>Only a builder for Build Action within <see cref="InteractiveCommandContainer{T}"./></summary>
+    public class InteractiveCommandContainerBuildActionContext<T>
+        where T : InteractiveCommandContext
+    {
+        private readonly HashSet<InteractiveCommandBuilder<T>> _builders;
+
+        internal InteractiveCommandContainerBuildActionContext() 
+        {
+            _builders = new HashSet<InteractiveCommandBuilder<T>>();
+        }
+
+        public void Add<U>() 
+            where U : InteractiveCommandBuilder<T>, new() => 
+            _builders.Add(new U());
+
+        internal void InjectInto(InteractiveCommandContainer<T> container)
+        {
+            foreach(var builder in _builders)
+            {
+                container._builders.Add(builder);
+            }
+        }
+    }
+}
